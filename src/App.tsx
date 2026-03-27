c5d69805635be01faa7099ab7c468750277878a0
maitamgk <maitamit062005@gmail.com>
2026-03-27 14:05:21 +0700

    fix(web): add ScrollToTop component to reset window scroll position on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import TeamPage from './pages/TeamPage';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
 
 function App() {
   const [scrolled, setScrolled] = React.useState(false);
@@ -26,6 +36,8 @@ function App() {
           <div className="aurora-blob blob-3"></div>
         </div>
         <div className="bg-grid"></div>
+        <ScrollToTop />
+        
         
         <Navbar scrolled={scrolled} />
         
